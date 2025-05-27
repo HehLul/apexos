d71d450e9083a575880fc6e34cf6bd781980bc4e
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2025-05-27 06:08:48 -0400

    feat: add nav component

---
+++ landingpage/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css";
+function Navbar() {
+  return (
+    <div className="navbar">
+      <div className="img">Logo</div>
+      <ul>
+        <a href="">Feature</a>
+        <a href="">Why ApexOS</a>
+        <a href="">How it works</a>
+        <a href="">Pricing</a>
+        <a href="">Home</a>
+      </ul>
+      <button>Get ApexOS</button>
+    </div>
+  );
+}
+export default Navbar;
