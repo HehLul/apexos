f1b19ec79c80632ec6f55f988b410dd42e332afd
Ahmed Salman <81371023+HehLul@users.noreply.github.com>
2025-06-07 13:26:39 -0400

    style: add link styles

--- landingpage/src/sections/New/New.jsx
+++ landingpage/src/sections/New/New.jsx
@@ -5,9 +5,12 @@ function New() {
       <h1>This is a New Section</h1>
       <h2>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Est quibusdam
-        in ea esse, nemo et ipsam officiis quia adipisci optio similique dolorum
-        vitae blanditiis ratione? Libero similique culpa ullam aperiam.
+        in ea esse, nemo et ipsam officiis quia adipisci{" "}
+        <a href="">Learn more</a> optio similique dolorum vitae blanditiis
+        ratione? Libero similique culpa ullam aperiam.
       </h2>
+
+      <a href=""></a>
     </div>
   );
 }
